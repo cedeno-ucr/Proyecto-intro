0eaab04906ea936dc2e4489ed05404b588251d7c
cedeno-ucr <christian.cedenomora@ucr.ac.cr>
2024-06-03 23:25:33 -0600

    Se añade el archivo de texto de países y una versión preliminar de los demás para poder realizar pruebas. Se añade la lectura de los contenidos de estos archivos a listas y la lista que contiene todos los contenidos Se empieza a trabajar en la función de selección de parámetros.

--- Proyecto.py
+++ Proyecto.py
@@ -1,8 +1,7 @@
-#Aquí inicia la vara esta
 def jugar():
     print('Trabajo en proceso')
 
-def parametros():
+def parametros(lista, vidas):
     print('Trabajo en proceso')
 
 def resultados():
@@ -10,26 +9,25 @@ def resultados():
 
 def ayuda(opcion):
     if opcion == 1:
-        print(f'Para jugar, una palabra aleatoria será elegida de entre la')
-        print(f'o las listas escogidas en Parámetros. Usted deberá digitar')
-        print(f'letra a letra las letras en la palabra en la cantidad de')
-        print(f'intentos establecida en Parámetros. De encontar todas las')
-        print(f'letras, ganará. Si no lo logra en la cantidad de intentos o')
-        print(f'menos, perderá.')
-        return
+        print('Para jugar, una palabra aleatoria será elegida de entre la')
+        print('o las listas escogidas en Parámetros. Usted deberá digitar')
+        print('letra a letra las letras en la palabra en la cantidad de')
+        print('intentos establecida en Parámetros. De encontar todas las')
+        print('letras, ganará. Si no lo logra en la cantidad de intentos o')
+        print('menos, perderá.')
+
     elif opcion == 2:
         print('Trabajo en progreso')
-        return
+
     elif opcion == 3:
-        print(f'En este menú se muestran la cantidad de partidas ganadas y')
-        print(f'perdidas en la sesión actual.')
-        return
+        print('En este menú se muestran la cantidad de partidas ganadas y')
+        print('perdidas en la sesión actual.')
+
     elif opcion == 4:
         print('Volviendo al menú principal.')
-        return
+
     else:
-        print(f'Error. Opción incorrecta')
-        return
+        print('Error. Opción incorrecta')
 
 
 
@@ -37,6 +35,28 @@ def acerca_de():
     print('Programa hecho por Andrés Cedeño, Alex Ulate, y Sergio [apellido de sergio]\ncomo proyecto del curso de Introducción a la Computación impartido por\nIgnacio Díaz en el primer semestre, 2024')
     return
 
+
+file_flwr = open("flores.txt", "r")
+list_flwr = file_flwr.readline().split()
+file_flwr.close()
+
+file_sprt = open("deportes.txt", "r")
+list_sprt = file_sprt.readline().split()
+file_sprt.close()
+
+file_anim = open("animales.txt", "r")
+list_anim = file_anim.readline().split()
+file_anim.close()
+
+file_ctry = open("paises.txt", "r")
+list_ctry = file_ctry.readline().split()
+file_ctry.close()
+
+list_all = list_anim+list_ctry+list_flwr+list_sprt
+
+lista_activa = list_all
+
+intentos = 10
 
 
 bandera_salida = False
@@ -55,7 +75,23 @@ while not bandera_salida:
     if opcion_menu == 1:
         jugar()
     elif opcion_menu == 2:
-        parametros()
+        print('Cambio de parámetros')
+        print('Digite el número correspondiente a lista que quiere usar.')
+        print('Listas:')
+        print('1. Flores')
+        print('2. Deportes')
+        print('3. Animales')
+        print('4. Países')
+        print('0. Lista Combinada')
+        numero_lista = int(input('Lista: '))
+
+        print('Digite el número correspondiente a la dificultad que quiera usar.')
+        print('1. Fácil')
+        print('2. Normal')
+        print('3. Difícil')
+        numero_dificultad = int(input('Dificultad: '))
+
+        parametros(numero_lista, numero_dificultad)
 
     elif opcion_menu == 3:
         resultados()
@@ -78,3 +114,5 @@ while not bandera_salida:
 
     else:
         print('Opción invalida')
+
+#Nota, el programa no lee caractéres especiales como tildes o la Ñ desde los archivos por un problema de codificaciión
